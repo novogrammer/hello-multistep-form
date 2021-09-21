a38b9e33ea4bb035350984bbefe3071b58882a4d
Yusuke Kawamoto <novogrammer@gmail.com>
2021-09-21 19:41:33 +0900

    セミコロンをつけておく。

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-app.use(express.urlencoded({ extended: true }))
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 app.post('/serverside-a', (req, res) => {
   console.log(req.body);
   res.json(req.body);
-})
+});
 // Start Listenings
-app.listen(3000, () => console.log('Listening on http://localhost:3000/...'))
+app.listen(3000, () => console.log('Listening on http://localhost:3000/...'));
 
